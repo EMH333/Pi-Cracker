877ad19a40089f6e80711478850028cd34ef955d
Ethan <ethohampton@gmail.com>
2016-01-12 18:26:19 -0800

    Added way to figure out what letter the value was equal to.

--- Pi Password Decoder/src/com/ethohampton/pi_decoder/Alphabet.java
+++ Pi Password Decoder/src/com/ethohampton/pi_decoder/Alphabet.java
@@ -1,30 +1,89 @@
 package com.ethohampton.pi_decoder;
 
 public class Alphabet {
-	int a = 1;
-	int b = 2;
-	int c = 3;
-	int d = 4;
-	int e = 5;
-	int f = 6;
-	int g = 7;
-	int h = 8;
-	int i = 9;
-	int j = 10;
-	int k = 11;
-	int l = 12;
-	int m = 13;
-	int n = 14;
-	int o = 15;
-	int p = 16;
-	int q = 17;
-	int r = 18;
-	int s = 19;
-	int t = 20;
-	int u = 21;
-	int v = 22;
-	int w = 23;
-	int x = 24;
-	int y = 25;
-	int z = 26;
+	static int a = 1;
+	static int b = 2;
+	static int c = 3;
+	static int d = 4;
+	static int e = 5;
+	static int f = 6;
+	static int g = 7;
+	static int h = 8;
+	static int i = 9;
+	static int j = 10;
+	static int k = 11;
+	static int l = 12;
+	static int m = 13;
+	static int n = 14;
+	static int o = 15;
+	static int p = 16;
+	static int q = 17;
+	static int r = 18;
+	static int s = 19;
+	static int t = 20;
+	static int u = 21;
+	static int v = 22;
+	static int w = 23;
+	static int x = 24;
+	static int y = 25;
+	static int z = 26;
+
+	public static String findLetter(int num) {
+		if (num == a) {
+			return "a";
+		} else if (num == b) {
+			return "b";
+		} else if (num == c) {
+			return "c";
+		} else if (num == d) {
+			return "d";
+		} else if (num == e) {
+			return "e";
+		} else if (num == f) {
+			return "f";
+		} else if (num == g) {
+			return "g";
+		} else if (num == h) {
+			return "h";
+		} else if (num == i) {
+			return "i";
+		} else if (num == j) {
+			return "j";
+		} else if (num == k) {
+			return "k";
+		} else if (num == l) {
+			return "l";
+		} else if (num == m) {
+			return "m";
+		} else if (num == n) {
+			return "n";
+		} else if (num == o) {
+			return "o";
+		} else if (num == p) {
+			return "p";
+		} else if (num == q) {
+			return "q";
+		} else if (num == r) {
+			return "r";
+		} else if (num == s) {
+			return "s";
+		} else if (num == t) {
+			return "t";
+		} else if (num == u) {
+			return "u";
+		} else if (num == v) {
+			return "v";
+		} else if (num == w) {
+			return "w";
+		} else if (num == x) {
+			return "x";
+		} else if (num == y) {
+			return "y";
+		} else if (num == z) {
+			return "z";
+		} else {
+			return null;
+		}
+
+	}
 }
--- Pi Password Decoder/src/com/ethohampton/pi_decoder/Pi_Decoder.java
+++ Pi Password Decoder/src/com/ethohampton/pi_decoder/Pi_Decoder.java
@@ -3,9 +3,11 @@ package com.ethohampton.pi_decoder;
 public class Pi_Decoder {
 
 	public String passwordToFind;
+	public static String answer = "test";
 	public long trysToFindPass;
 	public static String[] piInString;
 	public static int[] piNumbers;
+	
 
 	public static void main(String[] args) {
 
@@ -17,12 +19,13 @@ public class Pi_Decoder {
 		}
 
 		for (int i = 0; i < piNumbers.length; i++) {
+			String str = null;
 			int v = piNumbers[i];
-			int oldRange = (999 - 0);
-			int newRange = (24 - 0);
-			int newValue = (((v - 0) * newRange) / oldRange) + 0;
-			piNumbers[i] = newValue;
-			System.out.println(piNumbers[i]);
+			
+			str = Alphabet.findLetter(v);
+			
+			
+			answer.concat(str);
 		}
 
 	}
